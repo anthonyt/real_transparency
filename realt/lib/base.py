cf2f107214c914df4d7a3939a7a31bed9a37fa6d
Anthony Theocharis <anthony@simplestation.com>
2010-04-04 05:40:22 -0700

    Add an expose decorator.

--- realt/lib/base.py
+++ realt/lib/base.py
@@ -7,6 +7,8 @@ from pylons.templating import render_genshi as render
 
 from realt.model.meta import Session
 
+from pylons.decorators import jsonify
+
 class BaseController(WSGIController):
 
     def __call__(self, environ, start_response):
@@ -18,3 +20,25 @@ class BaseController(WSGIController):
             return WSGIController.__call__(self, environ, start_response)
         finally:
             Session.remove()
+
+
+def expose(template):
+    """Simple expose decorator for controller actions.
+
+    Takes a single template argument.
+    'string' for returning a simple string
+    'json' for returning the json encoding of a returned dict
+    any template for returning the rendered template with the local variables
+        set from a returned dict
+    """
+    def wrap(f):
+        if template == "json":
+            return jsonify(f)
+        elif template == "string":
+            return f
+        def wrapped_f(*args, **kwargs):
+            result = f(*args, **kwargs)
+            return render(template, extra_vars=result)
+        return wrapped_f
+    return wrap
+
