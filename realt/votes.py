64fef2a9e9ffb0f0ad5712dd3f1323351cb7cab8
Anthony Theocharis <anthony.theocharis@gmail.com>
2015-05-12 19:45:31 -0700

    Add some basic code for fetching votes.

--- realt/votes.py
+++ realt/votes.py
@@ -1,4 +1,6 @@
-
+"""
+doc doc doc
+"""
 __all__ = ['chamber_vote_details', 'votes_in_session']
 
 import urllib
@@ -296,3 +298,51 @@ def chamber_vote_details(parliament, session, vote_numbers=None):
 
     return cvdetails
 
+
+class XmlimportController(BaseController):
+
+    @expose('json')
+    def index(self, parliament, session, vote_number=None, limit=1):
+        if vote_number is not None:
+            vote_number = int(vote_number)
+        parliament = int(parliament)
+        session = int(session)
+        limit = int(limit)
+
+        data = self._pull_data(parliament, session, vote_number, limit)
+        return data
+
+    def _pull_data(self, parliament, session, vote_number, limit=1):
+        if vote_number:
+            vote_numbers = [vote_number]
+        else:
+            vote_numbers = votes_in_session(parliament, session)
+
+        if limit > 0 and len(vote_numbers) > limit:
+            vote_numbers = vote_numbers[:limit]
+
+        rawvotes = Session.query(RawHouseVote)\
+            .filter(RawHouseVote.parliament==parliament)\
+            .filter(RawHouseVote.session==session)\
+            .filter(RawHouseVote.number.in_(vote_numbers))\
+            .all()
+
+        found_numbers = [rv.number for rv in rawvotes]
+        new_vote_numbers = [n for n in vote_numbers if n not in found_numbers]
+
+        log.info('Found these RawHouseVotes in the database: %r', found_numbers)
+        log.info('Looking up these new RawHouseVotes: %r', new_vote_numbers)
+
+        cvds = chamber_vote_details(parliament, session, new_vote_numbers)
+        for cvd in cvds:
+            rv = RawHouseVote(cvd)
+            Session.add(rv)
+            rawvotes.append(rv)
+
+        results = []
+        for rv in rawvotes:
+            d = rv.to_dict()
+            d['date'] = d['date'].strftime('%y-%m-%d')
+            results.append(d)
+
+        return results
